90cd5ce65b70c5e4ddd738ffe4d659b3838ef083
Nhat Anh Nguyen <nhatanh.nguyen.2795@gmail.com>
2024-04-24 07:40:23 +0700

    add project conffig server and update code

--- btl-ptit/auth/src/main/java/vn/dev/na/auth/Main.java
+++ btl-ptit/auth/src/main/java/vn/dev/na/auth/Main.java
@@ -1,12 +1,14 @@
-package org.example;
+package vn.dev.na.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Main {
 	
-	public static void main(String[] args) {
+	public static void main(String []args) {
 		SpringApplication.run(Main.class, args);
 	}
+
 }
---
+++ config-server/src/main/java/vn/dev/na/config/server/handleExceptionInternal.java
@@ -0,0 +1,27 @@
+package vn.dev.na.config.server;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class handleExceptionInternal extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
+	protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+		String bodyOfResponse = "This should be application specific";
+		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+	}
+
+	
+	@ExceptionHandler(value = { RuntimeException.class, Exception.class })
+	protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
+		String bodyOfResponse = "This should be application specific";
+		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+	}
+
+}
