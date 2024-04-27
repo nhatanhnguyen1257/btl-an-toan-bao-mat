8c93fd83c2f4930cbc1171995237428a1ac2d567
Nhat Anh Nguyen <nhatanh.nguyen.2795@gmail.com>
2024-04-28 06:55:54 +0700

    fix bug

--- config-server/src/main/java/vn/dev/na/config/server/handleExceptionInternal.java
+++ config-server/src/main/java/vn/dev/na/config/server/handleExceptionInternal.java
@@ -1,5 +1,6 @@
 package vn.dev.na.config.server;
 
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
 
 @ControllerAdvice
 public class handleExceptionInternal extends ResponseEntityExceptionHandler {
@@ -18,10 +24,19 @@ public class handleExceptionInternal extends ResponseEntityExceptionHandler {
 	}
 
 	
-	@ExceptionHandler(value = { RuntimeException.class, Exception.class })
-	protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
-		String bodyOfResponse = "This should be application specific";
-		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
-	}
+//	@ExceptionHandler(value = { RuntimeException.class})
+//	protected ResponseEntity<Object> handleException(RuntimeException ex, WebRequest request) {
+//		String bodyOfResponse = "This should be application specific";
+//		return handleExceptionInternal(ex, bodyOfResponse, new HttpHeaders(), HttpStatus.CONFLICT, request);
+//	}
+	
+	@ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handle(Exception ex, HttpServletRequest request, HttpServletResponse response) {
+        if (ex instanceof NullPointerException) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+    }
+	
 
 }
